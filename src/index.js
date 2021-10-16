a5670936bafe840396b668dee9f38b809513c282
atsushi.isai <atsushi_i@branch-plus.co.jp>
2021-10-16 23:07:37 +0900

    fix portfolio

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { Header } from "./components/Header";
+import { Skills } from "./components/Skills";
+import { Contact } from "./components/Contact";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Header />
+    <Skills />
+    <Contact />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
